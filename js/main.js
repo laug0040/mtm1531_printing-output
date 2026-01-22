8a698cef8f223338840ecf550ed347b1ad5872f6
Korey Laughren Bellefeuille <laug0040@algonquinlive.com>
2026-01-22 12:37:35 -0500

    Completing Task 2

--- js/main.js
+++ js/main.js
@@ -21,3 +21,11 @@ console.log("a > b: " + (a > b))
 console.log("a < b: " + (a < b))
 console.log("!a && !c: " + (a && c))
 console.log("!a || !c: " + (a || c))
+
+// Task 2
+let first_name = "Korey"
+let last_name = "Laughren Bellefeuille"
+let email = "laug0040@algonquinlive.com"
+let output = "My name is " + first_name + " " + last_name + "." + " You can contact me at " + email + "."
+
+console.log(output)
